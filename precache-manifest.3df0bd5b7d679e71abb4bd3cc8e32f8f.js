ce2c428a530eb75257486c5f9f6bf055b1203b41
vkonapp <77061712+vkonapp@users.noreply.github.com>
2021-01-06 19:59:32 +0500

    Updates

---
+++ precache-manifest.3df0bd5b7d679e71abb4bd3cc8e32f8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a2380de2aaf609f80ea52c411c1e700",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2de556e827ceeab7b4d2",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "fb8167f0e1eae4fa942f",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "2de556e827ceeab7b4d2",
+    "url": "/static/js/2.8e14a0e4.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.8e14a0e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb8167f0e1eae4fa942f",
+    "url": "/static/js/main.02611ee0.chunk.js"
+  },
+  {
+    "revision": "b75330c9ec68c2e24146",
+    "url": "/static/js/runtime-main.2c5a7c8e.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
